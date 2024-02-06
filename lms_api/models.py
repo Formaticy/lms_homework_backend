ed45d0d2436501367c0df4b5e7c0e0bfdc2da21b
Михаил Калинин <m2112623@edu.misis.ru>
2024-02-06 23:20:32 +0300

    update models

--- lms_api/models.py
+++ lms_api/models.py
@@ -1,36 +1,15 @@
 from django.db import models
 
 
-class TaskType(models.Model):
-    type = models.CharField(max_length=30)
+class File(models.Model):
+    path = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    owner = models.ForeignKey("lms_api.Person", null=True, on_delete=models.SET_NULL)
 
     class Meta:
-        db_table = "TaskType"
-
-
-class Task(models.Model):
-    title = models.CharField(max_length=50)
-    max_points = models.FloatField()
-    text = models.TextField()
-
-    course = models.ForeignKey('lms_api.Course', null=True, on_delete=models.SET_NULL)
-    type = models.ForeignKey('lms_api.TaskType', on_delete=models.RESTRICT)
-    files = models.ManyToManyField('lms_api.File', db_table='FileForTask')
-
-    class Meta:
-        db_table = "Task"
-
-
-class Assignment(models.Model):
-    date_start = models.DateTimeField()
-    date_end = models.DateTimeField()
-
-    assigner = models.ForeignKey('lms_api.Person', null=True, on_delete=models.SET_NULL)
-    task = models.ForeignKey('lms_api.Task', null=True, on_delete=models.SET_NULL)
-    role = models.ForeignKey('lms_api.Role', null=True, on_delete=models.SET_NULL)
-
-    class Meta:
-        db_table = "Assignment"
+        db_table = "File"
 
 
 class PersonType(models.Model):
@@ -47,25 +26,12 @@ class Person(models.Model):
     phone = models.CharField(max_length=15)
     mail = models.EmailField()
 
-    type = models.ForeignKey('lms_api.PersonType', on_delete=models.RESTRICT)
-    courses = models.ManyToManyField('lms_api.Course', through='Role')
-    roles = models.ManyToManyField('lms_api.Role', db_table='Person_Roles')
+    type = models.ForeignKey("lms_api.PersonType", on_delete=models.RESTRICT)
+    courses = models.ManyToManyField("lms_api.Course", through="Role")
+    roles = models.ManyToManyField("lms_api.Role", db_table="Person_Roles", related_name='persons')
 
     class Meta:
         db_table = "Person"
-
-
-class Course(models.Model):
-    name = models.CharField(max_length=100)
-    date_start = models.DateTimeField()
-    date_end = models.DateTimeField()
-
-    owner = models.ForeignKey('lms_api.Person', null=True, on_delete=models.SET_NULL)
-    files = models.ManyToManyField('lms_api.File', db_table='FileForCourse')
-
-    class Meta:
-        db_table = "Course"
-
 
 
 class RoleType(models.Model):
@@ -76,44 +42,78 @@ class RoleType(models.Model):
 
 
 class Role(models.Model):
-    person = models.ForeignKey('lms_api.Person', null=True, on_delete=models.SET_NULL)
-    course = models.ForeignKey('lms_api.Course', null=True, on_delete=models.SET_NULL)
-    type = models.ForeignKey('lms_api.RoleType', on_delete=models.RESTRICT)
+    type = models.ForeignKey("lms_api.RoleType", on_delete=models.RESTRICT)
+    person = models.ForeignKey("lms_api.Person", null=True, on_delete=models.SET_NULL)
+    course = models.ForeignKey("lms_api.Course", null=True, on_delete=models.SET_NULL)
 
     class Meta:
         db_table = "Role"
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    owner = models.ForeignKey("lms_api.Person", null=True, on_delete=models.SET_NULL)
+    files = models.ManyToManyField("lms_api.File", db_table="FileForCourse")
+
+    class Meta:
+        db_table = "Course"
+
+
+class TaskType(models.Model):
+    type = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = "TaskType"
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+
+    type = models.ForeignKey("lms_api.TaskType", on_delete=models.RESTRICT)
+    files = models.ManyToManyField("lms_api.File", db_table="FileForTask")
+
+    class Meta:
+        db_table = "Task"
+
+
+class Assignment(models.Model):
+    max_points = models.FloatField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    assigner = models.ForeignKey("lms_api.Person", null=True, on_delete=models.SET_NULL)
+    task = models.ForeignKey("lms_api.Task", null=True, on_delete=models.SET_NULL)
+    role = models.ForeignKey("lms_api.Role", null=True, on_delete=models.SET_NULL)
+    course = models.ForeignKey("lms_api.Course", null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        db_table = "Assignment"
 
 
 class Result(models.Model):
     submit_date = models.DateTimeField()
     points = models.FloatField()
 
-    person = models.ForeignKey('lms_api.Person', null=True, on_delete=models.SET_NULL)
-    assignment = models.ForeignKey('lms_api.Assignment', null=True, on_delete=models.SET_NULL)
-    files = models.ManyToManyField('lms_api.File', db_table='FileForResult')
+    person = models.ForeignKey("lms_api.Person", null=True, on_delete=models.SET_NULL)
+    assignment = models.ForeignKey("lms_api.Assignment", null=True, on_delete=models.SET_NULL)
+    files = models.ManyToManyField("lms_api.File", db_table="FileForResult")
 
     class Meta:
         db_table = "Result"
 
 
-class File(models.Model):
-    path = models.CharField(max_length=100)
-    date = models.DateTimeField()
-
-    owner = models.ForeignKey('lms_api.Person', null=True, on_delete=models.SET_NULL)
-
-    class Meta:
-        db_table = "File"
-
-
 class Announcement(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     text = models.TextField()
 
-    sender = models.ForeignKey('lms_api.Person', null=True, on_delete=models.SET_NULL)
-    course = models.ForeignKey('lms_api.Course', null=True, on_delete=models.SET_NULL) # здесь в явном виде не задавал связь м2м (наверное, так норм)
-    files = models.ManyToManyField('lms_api.File', db_table='FileForAnnouncement')
+    sender = models.ForeignKey("lms_api.Person", null=True, on_delete=models.SET_NULL)
+    course = models.ForeignKey("lms_api.Course", null=True, on_delete=models.SET_NULL)
+    files = models.ManyToManyField("lms_api.File", db_table="FileForAnnouncement")
 
     class Meta:
         db_table = "Announcement"
